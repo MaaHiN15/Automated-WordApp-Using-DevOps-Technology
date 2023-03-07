bd6ac96b8ef5adef4ce4290c9bf7d8f89adba41d
MaaHiN15 <mahendranmaahin007@gmail.com>
2023-03-07 15:10:36 +0530

    lets start to dockerize

--- user/db.py
+++ user/db.py
@@ -1,9 +1,8 @@
 from pymongo import MongoClient
 import os
-db_address = os.environ['DB_ADDRESS']
 def db_connect():
     # client = MongoClient('mongodb+srv://maahin:admin@maahin-cluster.qsy8xid.mongodb.net/?retryWrites=true&w=majority')
-    client = MongoClient(f'{db_address}')
+    client = MongoClient('mongodb://maahin:admin@mongo:27017')
     # client.db.auth.drop()
     return client.db.auth
 db_connect()
