f0be2386c34365a13c4432917c15584bfbc225a0
MaaHiN15 <mahendranmaahin007@gmail.com>
2023-03-07 18:41:31 +0530

    k8s config and secret done

--- user/db.py
+++ user/db.py
@@ -1,7 +1,6 @@
 from pymongo import MongoClient
 import os
 def db_connect():
-    # client = MongoClient('mongodb+srv://maahin:admin@maahin-cluster.qsy8xid.mongodb.net/?retryWrites=true&w=majority')
     client = MongoClient(os.environ['DB_URL'])
     # client.db.auth.drop()
     return client.db.auth
