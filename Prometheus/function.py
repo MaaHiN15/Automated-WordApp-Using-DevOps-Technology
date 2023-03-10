5b088e67b6729f6de5ea7fe153a90ec3f6c9aafe
MaaHiN15 <mahendranmaahin007@gmail.com>
2023-03-10 20:19:20 +0530

    some try in prometheus using collectorRegistry

--- Prometheus/function.py
+++ Prometheus/function.py
@@ -1,9 +1,12 @@
-from prometheus_client import Counter, Gauge, Summary, Histogram
+from prometheus_client import Counter, Gauge, Summary, Histogram, CollectorRegistry
 
-count_home_page_metric = Counter('home_page_endpoint_request', 'Total request of home page endpoint')
+registry = CollectorRegistry()
 
-user_creation_metric = Counter('user_creation_total', 'Total number of user registered')
+count_home_page_metric = Counter('home_page_endpoint_request', 'Total request of home page endpoint', registry=registry)
 
-user_login_metric = Counter('total_login', 'Total number of login')
+user_creation_metric = Counter('user_creation_total', 'Total number of user registered', registry=registry)
 
-word_fetching_metric = Counter('word_fetching_metric', 'Total number of words fetching')
+user_login_metric = Counter('total_login', 'Total number of login', registry=registry)
+
+word_fetching_metric = Counter('word_fetching_metric', 'Total number of words fetching', registry=registry)
+
